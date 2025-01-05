9f57ce7ca9c5875fa1d781fc4b8f379a8717748e
sirisab <143996815+sirisab@users.noreply.github.com>
2025-01-05 22:46:36 +0100

    added badge count but with bug regarding updateing

--- frontend/src/Components/UI/Navbar.tsx
+++ frontend/src/Components/UI/Navbar.tsx
@@ -1,17 +1,51 @@
 import { NavLink } from 'react-router-dom';
 import styles from './navbar.module.css';
 import { HiMiniShoppingBag } from 'react-icons/hi2';
+import { LoadArtworks } from '../../Utilities/LoadArtworks';
+import { ShoppingcartList } from '../ShoppingcartList';
+import { useEffect, useState } from 'react';
+import { IArtwork } from '../../Models/IArtwork';
 
 const Navbar = () => {
+  const [itemCount, setItemCount] = useState(0);
+
+  const updateCartBadge = async () => {
+    const data = await LoadArtworks('/shoppingcart');
+    console.log('Polling data:', data); // Logga data för att se vad som hämtas
+
+    setItemCount(data.length);
+  };
+
+  useEffect(() => {
+    updateCartBadge();
+
+    const interval = setInterval(updateCartBadge, 10000);
+
+    return () => clearInterval(interval);
+  }, ['/shoppingcart']);
+
   return (
     <nav className={styles.navbar}>
-      <NavLink to='/'>
-        <h1>Pastellbutiken</h1>
-      </NavLink>
+      <div>
+        <NavLink to='/'>
+          <h1>Pastellbutiken</h1>
+        </NavLink>
+      </div>
 
-      <NavLink to='/shoppingcart'>
-        <HiMiniShoppingBag className={styles.badge} size={30} />
-      </NavLink>
+      <div>
+        <div>
+          <NavLink to='/shoppingcart'>
+            <HiMiniShoppingBag size={30} />
+          </NavLink>
+        </div>
+        <div
+          className={`${styles.badge} ${
+            itemCount === 0 ? 'styles.hidden' : ''
+          }}`}
+        >
+          {itemCount}
+        </div>
+      </div>
     </nav>
   );
 };
